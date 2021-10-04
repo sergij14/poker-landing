3fef7a8d98cb221ca2b75c63c06a45d2be1cf47e
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-10-03 21:07:46 -0700

    added inline font

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -4,7 +4,12 @@ import { useAppContext } from "../context";
 const Wrapper = ({ children }) => {
   const { font } = useAppContext();
   return (
-    <div className={`bg-landing-bg font-bpg text-white fontF-${font}`}>
+    <div
+      className="bg-landing-bg font-bpg text-white"
+      style={{
+        fontFamily: `${font}`,
+      }}
+    >
       {children}
     </div>
   );
